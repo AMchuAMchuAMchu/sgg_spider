846bde0fe7bf7c7eca9279ecfa455199a23e8f57
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-03 21:35:21 +0800

    shino_hecate

--- com/shino_hecate/_55_urllib_网页图片视频下载.py
+++ com/shino_hecate/_55_urllib_网页图片视频下载.py
@@ -10,7 +10,6 @@ import urllib.request as request
 # response01 = request.urlretrieve(url='http://www.baidu.com',filename='baidu.html')
 # response01 = request.urlretrieve(url='https://bkimg.cdn.bcebos.com/pic/aa18972bd40735fae6cd10d7c01818b30f2442a7a20e?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2UxNTA=,g_7,xp_5,yp_5',filename='LiSA.jpg')
 # response01 = request.urlretrieve(url='https://vd3.bdstatic.com/mda-ngr1eeg61hyxqv4p/hd/h264/1658797220132489707/mda-ngr1eeg61hyxqv4p.mp4',filename='LiSA.mp4')
-response01 = request.urlretrieve(url='https://www.bilibili.com/video/BV1xS4y1B7Db?t=3.6',filename='gold.mp4')
 
 
 
