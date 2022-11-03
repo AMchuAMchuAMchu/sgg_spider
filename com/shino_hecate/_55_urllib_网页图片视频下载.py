34e3b2afa6f4b0664c986e9126af4cde5320508e
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-03 20:49:26 +0800

    shino_hecate

---
+++ com/shino_hecate/_55_urllib_网页图片视频下载.py
@@ -0,0 +1,14 @@
+# Description ==> TODO
+# BelongsProject ==> sgg_spider
+# BelongsPackage ==> 
+# Version ==> 1.0
+# CreateTime ==> 2022-11-03 20:48:04
+# Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+
+import urllib.request as request
+
+response01 = request.urlretrieve(url='http://www.baidu.com',filename='baidu.html')
+
+
+
+
