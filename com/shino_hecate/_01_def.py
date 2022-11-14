e6c9c094d1d4bcf905b420068c5d4071cd0b6aa2
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-14 20:06:26 +0800

    shino_hecate

--- com/shino_hecate/_01_def.py
+++ com/shino_hecate/_01_def.py
@@ -5,7 +5,15 @@
 # CreateTime ==> 2022-10-31 08:45:01
 # Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
 
-def sum(num01, num02):
-    return num01 + num02
+# def sum(num01, num02):
+#     return num01 + num02
+#
+# print(sum(100, 99))
 
-print(sum(100, 99))
+import  numpy as np
+file = open(file='d:/twitter_comment.txt',encoding='utf-8',mode='w')
+for i in range(1,101,1):
+    res = '第',i-4,'条','==>'
+    file.writelines(res)
+
+
--- com/twitter_comment/demo.py
+++ com/twitter_comment/demo.py
@@ -11,17 +11,29 @@ import requests
 from bs4 import BeautifulSoup
 
 
-result01 = requests.request(method='get',url='https://explorer.altmetric.com/details/77676422/twitter/page:2?_pjax=.content-panel&_pjax=.content-panel')
+file = open(file='d:/twitter_comment.txt',encoding='utf-8',mode='w')
+for i in range(1,101,1):
+    print('====>',i)
+    result01 = requests.request(method='get',
+                                url='https://explorer.altmetric.com/details/82549788/twitter/page:%d'%(i))
+                                # 1.0 url='https://explorer.altmetric.com/details/77676422/twitter/page:%d'%(i))
+                                # 2.0 'https://explorer.altmetric.com/details/94531651/twitter/page:2?'
+                                # 3.0 'https://explorer.altmetric.com/details/91957925/twitter/page:2?_pjax=.content-panel'
+                                # 4.0 'https://explorer.altmetric.com/details/77699394/twitter/page:2?_pjax=.content-panel'
+                                # 5.0 'https://explorer.altmetric.com/details/82549788/twitter/page:2?_pjax=.content-panel'
+    result01.encoding = 'utf-8'
 
-result01.encoding = 'utf-8'
+    result002 = BeautifulSoup(result01.text, 'html.parser')
+    i = 0
+    for item in result002.select('.summary'):
+        i += 1
+        if i >= 5:
+            res = '第%d条=>%s'%(i-4,item.text)
+            file.writelines(res)
+            file.write('\n')
+            print('第',i-4,'条','==>',item.text)
 
-print(result01.ok)
 
-result002 = BeautifulSoup(result01.text,parser='html.parser')
-i = 0
-for item in result002.select('.summary'):
-    i += 1
-    print(i,'==>',item)
 
 
 
