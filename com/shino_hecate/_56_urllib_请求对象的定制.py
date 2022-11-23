a3b176fcc8e7f896b3df0f24bd54295653f3a82b
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-23 20:06:44 +0800

    shino_hecate

--- com/shino_hecate/_56_urllib_请求对象的定制.py
+++ com/shino_hecate/_56_urllib_请求对象的定制.py
@@ -7,9 +7,18 @@
 
 from urllib import request
 
-result01 = request.urlopen('https://www.baidu.com')
+headers = {
+'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+}
 
-content01 = result01.read().deocde('utf-8')
+url='https://www.baidu.com'
+
+request01 = request.Request(url=url,headers=headers)
+
+
+result01 = request.urlopen(request01)
+
+content01 = result01.read().decode('utf-8')
 
 print(content01)
 
