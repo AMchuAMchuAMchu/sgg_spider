89daae004d4ef08052b9e5e354e4efcc3e063442
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-24 19:48:54 +0800

    shino_hecate

--- com/shino_hecate/_59_urllib_post请求百度翻译.py
+++ com/shino_hecate/_59_urllib_post请求百度翻译.py
@@ -8,20 +8,22 @@
 
 from urllib import request
 from urllib import parse
+import json
 
-url = 'https://fanyi.baidu.com'
+
+url = 'https://fanyi.baidu.com/sug'
 
 headers = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35'
 }
 
 data = {
-    'kw':'spider'
+    'kw': 'spider'
 }
 
-data = parse.urlencode(data).encode('utf-8')
+data01 = parse.urlencode(data).encode('utf-8')
 
-request01 = request.Request(url=url,data=data,headers=headers)
+request01 = request.Request(url=url, data=data01, headers=headers)
 
 result01 = request.urlopen(request01)
 
@@ -30,4 +32,6 @@ result02 = result01.read().decode('utf-8')
 print(result02)
 
 
+content03 = json.loads(result02)
 
+print(content03)
---
+++ com/shino_hecate/_60_urllib_post请求百度翻译之详细翻译.py
@@ -0,0 +1,79 @@
+# Description ==> TODO
+# BelongsProject ==> sgg_spider
+# BelongsPackage ==> 
+# Version ==> 1.0
+# CreateTime ==> 2022-11-23 20:43:00
+# Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+
+
+from urllib import request
+from urllib import parse
+import json
+
+url = 'https://fanyi.baidu.com/v2transapi?from=en&to=zh'
+
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+}
+
+data_origin = {
+'from':'en',
+'to':'zh',
+'query':'love',
+'transtype':'realtime',
+'simple_means_flag':'3',
+'sign':'198772.518981',
+'token':'6d788ed1d5bae487e2e6bfdd5dab50d0',
+'domain':'common'
+}
+
+data = parse.urlencode(data_origin).encode('utf-8')
+
+request01 = request.Request(url=url,headers=headers,data=data)
+
+result01 = request.urlopen(request01)
+
+content01 = result01.read().decode('utf-8')
+
+print(content01)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
