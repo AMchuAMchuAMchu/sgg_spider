94ab2ec4b261ea17a58f27b935d5a95cf092b3dc
AMchuAMchuAMchu <2466130840@qq.com>
2022-12-14 19:35:05 +0800

    shino_hecate

---
+++ com/shino_hecate/_01_scrapy/_91_scrapy基本使用/scrapy_baidu_091/scrapy_baidu_091/spiders/baidu.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class BaiduSpider(scrapy.Spider):
+    name = 'baidu'
+    allowed_domains = ['www.baidu.com']
+    start_urls = ['http://www.baidu.com/']
+
+    def parse(self, response):
+        print('诗乃.......kirito....桐谷和人.....')
